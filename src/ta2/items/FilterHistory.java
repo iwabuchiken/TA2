282b3d58fa2db834590207adff655b4d3be5989f
iwabuchiken <iwabuchi.k.2010@gmail.com>
2014-10-13 12:45:21 +0900

    D-19
    v-2.1
    ---
    1. model
    
        ==> done

---
+++ src/ta2/items/FilterHistory.java
@@ -0,0 +1,111 @@
+package ta2.items;
+
+public class FilterHistory {
+
+	// id in database
+	long db_Id;
+
+	String created_at;
+	
+	String modified_at;
+
+	String keywords;
+	
+	String operator;
+
+	public FilterHistory(Builder builder) {
+		// TODO Auto-generated constructor stub
+		
+		this.db_Id	= builder.db_Id;
+
+		this.created_at	= builder.created_at;
+		
+		this.modified_at	= builder.modified_at;
+
+		this.keywords	= builder.keywords;
+		
+		this.operator	= builder.operator;
+
+	}
+
+	public long getDb_Id() {
+		return db_Id;
+	}
+
+	public String getCreated_at() {
+		return created_at;
+	}
+
+	public String getModified_at() {
+		return modified_at;
+	}
+
+	public String getKeywords() {
+		return keywords;
+	}
+
+	public String getOperator() {
+		return operator;
+	}
+
+	public void setDb_Id(long db_Id) {
+		this.db_Id = db_Id;
+	}
+
+	public void setCreated_at(String created_at) {
+		this.created_at = created_at;
+	}
+
+	public void setModified_at(String modified_at) {
+		this.modified_at = modified_at;
+	}
+
+	public void setKeywords(String keywords) {
+		this.keywords = keywords;
+	}
+
+	public void setOperator(String operator) {
+		this.operator = operator;
+	}
+	
+	public static class Builder {
+		
+		// id in database
+		long db_Id;
+
+		String created_at;
+		
+		String modified_at;
+
+		String keywords;
+		
+		String operator;
+
+		public FilterHistory build() {
+			
+			return new FilterHistory(this);
+		}
+		
+		public Builder setDb_Id(long db_Id) {
+			this.db_Id = db_Id; return this;
+		}
+
+		public Builder setCreated_at(String created_at) {
+			this.created_at = created_at; return this;
+		}
+
+		public Builder setModified_at(String modified_at) {
+			this.modified_at = modified_at; return this;
+		}
+
+		public Builder setKeywords(String keywords) {
+			this.keywords = keywords; return this;
+		}
+
+		public Builder setOperator(String operator) {
+			this.operator = operator; return this;
+		}
+
+	}//public static class Builder
+	
+}//public class FilterHistory
